dcdf9fef119ab8822efdfcf0f819a01c04baa742
Saket Pareek <saket.pareek90@gmail.com>
2018-09-11 00:53:11 +0530

    Move enemies across screen with update function

--- js/app.js
+++ js/app.js
@@ -14,9 +14,11 @@ var Enemy = function() {
 // Update the enemy's position, required method for game
 // Parameter: dt, a time delta between ticks
 Enemy.prototype.update = function(dt) {
-    // You should multiply any movement by the dt parameter
-    // which will ensure the game runs at the same speed for
-    // all computers.
+  if (this.x >= 505) {
+    this.x = 0;
+    this.movement = Math.floor(Math.random() * 400) + 200;
+  }
+  this.x += this.movement * dt;
 };
 
 // Draw the enemy on the screen, required method for game
