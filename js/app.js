9a9dd7d97dd212f7e0262e56cdd3f556a4efba88
Saket Pareek <saket.pareek90@gmail.com>
2018-09-11 00:42:52 +0530

    Create array to store enemies

--- js/app.js
+++ js/app.js
@@ -36,6 +36,7 @@ var Player = function() {
 // Place all enemy objects in an array called allEnemies
 // Place the player object in a variable called player
 
+let allEnemies = [];
 
 
 // This listens for key presses and sends the keys to your
